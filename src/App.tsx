25dd2f0d5372bd44b144b1b907b728e863a752aa
Thomas Woroniak <thomasworoniak@gmail.com>
2026-02-16 14:10:53 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import StarRating from './components/StarRating';
+
+function App() {
+  return (
+    <>
+      <div>Hello World!</div>
+      <StarRating maxRating={10} color='#fcc419' />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/StarRating.tsx
@@ -0,0 +1,89 @@
+import { useState, type SetStateAction } from 'react';
+import { Star } from 'lucide-react';
+
+type StarRatingProps = {
+  maxRating?: number;
+  rating?: number;
+  color?: string;
+  size?: number | string;
+};
+
+const containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.5rem',
+};
+
+const starContainerStyle = {
+  display: 'flex',
+  gap: '0.25rem',
+};
+
+const textStyle = {
+  lineHeight: '1',
+  margin: 0,
+};
+
+const starStyle = {
+  width: '24px',
+  height: '24px',
+  display: 'block',
+  cursor: 'pointer',
+};
+
+const StarItem = ({
+  onRate,
+  full,
+  color,
+  onHoverIn,
+  onHoverOut,
+}: {
+  onRate: () => void;
+  full: boolean;
+  color: string;
+  onHoverIn: () => void;
+  onHoverOut: () => void;
+}) => (
+  <span
+    role='button'
+    style={starStyle}
+    onClick={onRate}
+    onMouseEnter={onHoverIn}
+    onMouseLeave={onHoverOut}
+  >
+    {full ? (
+      <Star fill={color} strokeWidth={1.5} color={color} />
+    ) : (
+      <Star strokeWidth={1.5} color={color} />
+    )}
+  </span>
+);
+
+const StarRating = ({ maxRating = 3, color = '#fcc419' }: StarRatingProps) => {
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
+
+  const handleRating = (rating: SetStateAction<number>) => {
+    setRating(rating);
+  };
+
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <StarItem
+            key={i}
+            color={color}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+          />
+        ))}
+      </div>
+      <p style={{ ...textStyle, color }}>{tempRating || rating || ''}</p>
+    </div>
+  );
+};
+
+export default StarRating;
