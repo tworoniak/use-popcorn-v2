6b8df327e85927f5f982e4d689b6545764445c23
Thomas Woroniak <thomasworoniak@gmail.com>
2026-02-21 17:03:37 -0600

    add Spinner

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,5 +1,16 @@
+import { ClipLoader } from 'react-spinners';
+
 type LoaderProps = { text?: string };
 
 export default function Loader({ text = 'Loading...' }: LoaderProps) {
-  return <p className='loader'>{text}</p>;
+  return (
+    <>
+      <p className='loader'>{text}</p>
+      <ClipLoader />;
+    </>
+  );
 }
+
+// export default function Loader() {
+//   return <ClipLoader />;
+// }
